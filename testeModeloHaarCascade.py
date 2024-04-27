fb433e796501f292d419a92ae4ac029caf689590
WellingtonDev25 <56238012+WellingtonDev25@users.noreply.github.com>
2024-04-27 14:24:12 -0300

    deteccao com haarcascade mediapipe sem suporte 3.6

--- testeModeloHaarCascade.py
+++ testeModeloHaarCascade.py
@@ -1,13 +1,8 @@
 import numpy as np
 import cv2
 from keras.models import load_model
-import mediapipe as mp
 
-face = mp.solutions.face_detection
-Face = face.FaceDetection()
-mpDwaw = mp.solutions.drawing_utils
-i = cv2
-cap = cv2.VideoCapture('Videos/vd (4).mp4')
+cap = cv2.VideoCapture('Videos/vd (1).mp4')
 
 model = load_model('modelo.h5')
 
@@ -31,7 +26,7 @@ def getCalssName(classNo):
     elif classNo == 1:
         return 'Nojo'
     elif classNo == 2:
-        return 'Raiva'
+        return 'Medo'
     elif classNo == 3:
         return 'Felicidade'
     elif classNo == 4:
@@ -41,18 +36,17 @@ def getCalssName(classNo):
     elif classNo == 6:
         return 'Surpreso'
 
+classificador = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
 while True:
     success, imgOrignal = cap.read()
 
-    imgRGB = cv2.cvtColor(imgOrignal, cv2.COLOR_BGR2RGB)
-    results = Face.process(imgRGB)
-    facesPoints = results.detections
-    hO, wO, _ = imgRGB.shape
-    if facesPoints:
-        for id, detection in enumerate(facesPoints):
-            #mpDwaw.draw_detection(img, detection)
-            bbox = detection.location_data.relative_bounding_box
-            x,y,w,h = int(bbox.xmin*wO),int(bbox.ymin*hO),int(bbox.width*wO),int(bbox.height*hO)
+    imgGray = cv2.cvtColor(imgOrignal, cv2.COLOR_BGR2GRAY)
+    facesPoints = classificador.detectMultiScale(imgGray, scaleFactor=1.5, minSize=(50, 50))
+    hO, wO, _ = imgOrignal.shape
+
+    if len(facesPoints)>=1:
+        for (x,y,w,h) in facesPoints:
             imagemFace = imgOrignal[y-30:y+h,x:x+w]
             img = np.asarray(imagemFace)
             img = cv2.resize(img, (48, 48))
@@ -63,16 +57,16 @@ while True:
             indexVal = np.argmax(predictions)
             probabilityValue = np.amax(predictions)
             print(indexVal,probabilityValue)
-            if probabilityValue >0.30:
-                cv2.rectangle(imgOrignal, (x, y), (x + w, y + h), (0,255,0), 18)
+            if probabilityValue >0.20:
+                cv2.rectangle(imgOrignal, (x, y), (x + w, y + h), (0,255,0), 3)
 
-                cv2.putText(imgOrignal, str(getCalssName(indexVal)), (x, y-50), cv2.FONT_HERSHEY_SIMPLEX, 4,
-                            (0,255,0), 10, cv2.LINE_AA)
+                cv2.putText(imgOrignal, str(getCalssName(indexVal)), (x, y-50), cv2.FONT_HERSHEY_SIMPLEX, 2,
+                            (0,255,0), 8, cv2.LINE_AA)
 
-                cv2.putText(imgOrignal, str(round(probabilityValue * 100, 2)) + "%", (x, y-20), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,255,0), 4,
+                cv2.putText(imgOrignal, str(round(probabilityValue * 100, 2)) + "%", (x, y-20), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,255,0), 2,
                             cv2.LINE_AA)
 
-    cv2.imshow("Result", cv2.resize(imgOrignal,(0,0),None,0.40,0.40))
+    cv2.imshow("Result",imgOrignal)
     #cv2.imshow("Face", imagemFace)
     cv2.waitKey(15)
 
